9571236ca69216f1ebaf5700093c679653a3c4b2
Darren Roscoe <darrenroscoe@gmail.com>
2018-10-19 05:15:25 +0000

    Prepare secrets for deployment
    - Also use the same secret technique for dev db password for convenience

--- config/dev.exs
+++ config/dev.exs
@@ -66,10 +66,21 @@ config :phoenix, :stacktrace_depth, 20
 # Initialize plugs at runtime for faster development compilation
 config :phoenix, :plug_init_mode, :runtime
 
+get_secret = fn name ->
+  base = Path.expand("~/.config/task_manager")
+  File.mkdir_p!(base)
+  path = Path.join(base, name)
+  unless File.exists?(path) do
+    secret = Base.encode16(:crypto.strong_rand_bytes(32))
+    File.write!(path, secret)
+  end
+  String.trim(File.read!(path))
+end
+
 # Configure your database
 config :task_manager, TaskManager.Repo,
-  username: "task_manager",
-  password: "uapeey2Eecah",
+  username: "task_manager_dev",
+  password: get_secret.("dev_db_pass"),
   database: "task_manager_dev",
   hostname: "localhost",
   pool_size: 10
--- config/test.exs
+++ config/test.exs
@@ -9,10 +9,21 @@ config :task_manager, TaskManagerWeb.Endpoint,
 # Print only warnings and errors during test
 config :logger, level: :warn
 
+get_secret = fn name ->
+  base = Path.expand("~/.config/task_manager")
+  File.mkdir_p!(base)
+  path = Path.join(base, name)
+  unless File.exists?(path) do
+    secret = Base.encode16(:crypto.strong_rand_bytes(32))
+    File.write!(path, secret)
+  end
+  String.trim(File.read!(path))
+end
+
 # Configure your database
 config :task_manager, TaskManager.Repo,
-  username: "task_manager",
-  password: "uapeey2Eecah",
+  username: "task_manager_dev",
+  password: get_secret.("dev_db_pass"),
   database: "task_manager_test",
   hostname: "localhost",
   pool: Ecto.Adapters.SQL.Sandbox
