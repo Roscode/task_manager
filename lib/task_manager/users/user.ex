410c85d60cf01c9335b039a2e8f6dbfbdc377348
Darren Roscoe <darrenroscoe@gmail.com>
2018-10-26 02:22:29 +0000

    Fix user schema warning

--- lib/task_manager/users/user.ex
+++ lib/task_manager/users/user.ex
@@ -7,7 +7,7 @@ defmodule TaskManager.Users.User do
   schema "users" do
     field :admin, :boolean, default: false
     field :email, :string
-    has_many :tasks, Task
+    has_many :tasks, Task, foreign_key: :assignee_id
 
     timestamps()
   end
